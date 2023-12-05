00da8e5a5af9b67d661f40bfc5f49fb00680bfcb
Gianluca Bueno <gianbuenor@gmail.com>
2023-12-05 14:39:21 -0300

    Tests

---
+++ src/test/java/com/system/platform/controllers/UserControllerTest.java
@@ -0,0 +1,36 @@
+package com.system.platform.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserControllerTest {
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void insert() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void updatePassword() {
+    }
+
+    @Test
+    void updateStatus() {
+    }
+}
